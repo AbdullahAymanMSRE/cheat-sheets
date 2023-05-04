8567b4a9ef5b3ef0f5a658553df579f1a421058c
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2023-05-04 16:06:14 +0300

    table and alter commands

--- sheet.sql
+++ sheet.sql
@@ -8,6 +8,12 @@ DROP DATABASE IF EXISTS dbName;
 -- to select specific database:
 USE dbName;
 
+-- to list extisting databases:
+SHOW DATABASES;
+
+-- to search for a database with specific name:
+SHOW DATABASES LIKE 'dbname';
+
 ------------------------------------------
 -- DATATYPES:
 
@@ -47,3 +53,56 @@ USE dbName;
 
 ---ENUM => Select only one from some values
 ---SET => Select one or more value from some values
+------------------------------------------
+-- TABLES
+
+--> Create Table:
+CREATE TABLE IF NOT EXISTS tableName(
+	/*
+	in this form: <fieldName> <dataType>(<size>),
+	...
+	 */
+	test INT(11),
+	test2 VARCHAR(255)
+);
+
+--> To show table structure there are three ways:
+DESCRIBE tableName; -- 1
+SHOW COLUMNS FROM tableName; -- 2
+SHOW FIELDS FROM tableName -- 3
+
+--> To show tables in the database:
+SHOW TABLE STATUS;
+
+--> To get the code of creating a table:
+SHOW CREATE TABLE tableName;
+
+--> To Delete a table:
+DROP TABLE IF EXISTS tableName;
+
+--> To rename a table (2 ways):
+RENAME TABLE oldName TO newName /*, ... */; -- 1
+ALTER TABLE oldName RENAME newName; -- 2
+
+
+--> Change table storage engin:
+ALTER TABLE tableName ENGIN = InnoDB;
+
+-- ALTER COMMAND
+-- add column:
+ALTER TABLE tableName ADD /* <columnName> <dataType>(<size>)*/ testColumn VARCHAR(255) /* AFTER otherColumnName | FIRST */;
+
+-- delete column:
+ALTER TABLE tableName DROP columnName;
+
+-- change column:
+ALTER TABLE tableName CHANGE oldColumnName newColumnName /*<dataType>(<size>)*/ VARCHAR(255) /*... Other properties or change order*/
+
+-- modify table
+ALTER TABLE tableName MODIFY columnName /*<newDataType>(<size>)*/ VARCHAR(255);
+
+-- NOTE: MODIFY and CHANGE can be used together separated by commas
+
+-- change char-set of all columns in a table:
+ALTER TABLE tableName CONVERT TO CHARACTER SET /*<new-char-set>*/ utf8;
+
